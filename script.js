44fa9eb9aca8ee217b22a3557576dc643485be32
Shivam Sahani <shivamsahani8934@gmail.com>
2026-03-19 09:27:04 +0530

    Add Latest_job function to manage page display

---
+++ script.js
@@ -0,0 +1,6 @@
+function Latest_job(){
+    document.getElementById("header").style.display = "none";
+    document.getElementById("job_box").style.display = "none";
+    document.getElementById("about").style.display = "none";
+    document.getElementById("page2").style.display = "block";
+}
